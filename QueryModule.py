fef2b6e1ea26b9a65ad80a1c6ffb02771a1671aa
Ruben Monferrer <rubenmonfe@gmail.com>
2019-03-07 19:16:04 +0100

    Added comments for easy understanding

--- QueryModule.py
+++ QueryModule.py
@@ -1,68 +1,45 @@
 import pymysql
 
+
+# This function returns a dictionary containing all the needed info to start the GUI
 def obtainFilterDict():
 	# Create a connection object
+	dbServerName = "127.0.0.1"
+	dbUser = "root"
+	dbPassword = "1234"
+	dbName = "sample"
+	charSet = "utf8mb4"
+	#cursorType = pymysql.cursors.DictCursor
 
-	dbServerName    = "127.0.0.1"
-
-	dbUser          = "root"
-
-	dbPassword      = "1234"
-
-	dbName          = "world"
-
-	charSet         = "utf8mb4"
-
-	cursorType      = pymysql.cursors.DictCursor
-
-
-	connectionObject   = pymysql.connect(host=dbServerName, user=dbUser, password=dbPassword,
+	connectionObject = pymysql.connect(host=dbServerName, user=dbUser, password=dbPassword,
 
 	                                     db=dbName, charset=charSet)#,cursorclass=cursorType)
 
-
 	try:
-
 		# Create a cursor object
-
 		cursorObject        = connectionObject.cursor()
 
-
-
 	    # SQL query string
-
-		#obtain all column names in table city
-		#sqlQuery            = "select column_name from information_schema.columns where table_name = 'city'"
-
-		#obtain all column names in all tables (column,table)
-		#sqlQuery            = "select table_name, column_name from information_schema.columns"
-
-		#obtain all column names in city table (column,table)
-		sqlQuery            = "select table_name, column_name from information_schema.columns where table_name = 'city'"
-
-
-		#sqlQuery            = "select * from city
+		# Obtain all column names in employeesample table in format (table,name)
+		sqlQuery = "select table_name, column_name from information_schema.columns where table_name = 'employeesample'"
 
 	    # Execute the sqlQuery
-
 		cursorObject.execute(sqlQuery)
 
-		#Fetch all the rows
+		# Fetch all the rows
+		rows = cursorObject.fetchall()
 
-		rows                = cursorObject.fetchall()
-
-		print(rows)
-
+		# Create dictionary to store db info
+		# Columns as keys and list of distinct values for the column as value
 		filterDict = {}
-
 		for row in rows:
 			table_name = row[0]
 			column_name = row[1]
-			sqlQuery = "select distinct {0} from {1}".format(column_name, table_name)
+			sqlQuery = "select distinct {0} from {1} order by {0}".format(column_name, table_name)
 			cursorObject.execute(sqlQuery)
 			options                = cursorObject.fetchall()
 			filterDict[column_name] = options 
-
+		
 		return filterDict
 
 	except Exception as e:
@@ -72,5 +49,3 @@ def obtainFilterDict():
 	finally:
 
 		connectionObject.close()
-
-
