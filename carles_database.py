8f98b1ce138fc2b77bb784d3601b5f672fea9952
Carles Bertran <cbertri@gmail.com>
2019-03-13 11:35:14 +0000

    SQL inserts model 3

--- carles_database.py
+++ carles_database.py
@@ -31,13 +31,16 @@ else:
     print ('You are currently disconnected')
 
 
-i=0
-parameters = [ ]
 cursor.execute("SET SQL_SAFE_UPDATES=0;") #later we wont use it but for now its useful to avoid overwriting data
 cursor.execute("DELETE FROM PMT;")
 cursor.execute("DELETE FROM Video;")
 
-
+idTS = 0
+idPMT = 0
+idStream = 0
+idVideo = 0
+idAudio = 0
+idSubtitle = 0
 
 for xml_name in (xml_list):
         fullname = os.path.join(final_xml_path, xml_name)
@@ -52,79 +55,88 @@ for xml_name in (xml_list):
         #TOT SECTION
         for tot in root.findall('{http://www.streamanalyser.com/schema}TOT'):
             recording_time = tot.find('{http://www.streamanalyser.com/schema}UTC_time').text
+        country_code = 'GER'
+        insertStatement = "INSERT INTO TS (Recording_Date, Country_Code) VALUES ({0}, {1})".format(recording_time, country_code)
+        cursor.execute(insertStatement)
+        idTS+=1
         #PMT SECTION
         for pmt in root.findall('{http://www.streamanalyser.com/schema}PMT'):
             print ('########### START PMT ##########')
-            pid = hex(int(pmt.find('{http://www.streamanalyser.com/schema}PID').text,16))
-            idTable = hex(int(pmt.find('{http://www.streamanalyser.com/schema}table_id').text,16))
-            print ('PID: ' + pid)
-            print ('table id: ' + idTable)
-
-            parameters.append([]) 
-            parameters[i].append(int(pid, 16))
+            pid = int(pmt.find('{http://www.streamanalyser.com/schema}PID').text,16)
+            print ('PID: ' + str(pid))
+            print ('idTS: ' + str(idTS))
+            insertStatement = "INSERT INTO PMT (PIDNumber, TS_identifierTS) VALUES ({0}, {1})".format(pid, idTS)
+            cursor.execute(insertStatement)
+            idPMT +=1
 
             for streams in pmt.findall('{http://www.streamanalyser.com/schema}Streams'):
                 for stream in streams.findall('{http://www.streamanalyser.com/schema}Stream'):
                     print ('###### START STREAM ######')
-                    stream_type =  hex(int(stream.find('{http://www.streamanalyser.com/schema}stream_type').text,16))
-                    elementary_PID =  hex(int(stream.find('{http://www.streamanalyser.com/schema}elementary_PID').text,16))
-                    print ('stream_type: ' + stream_type)
-                    print ('elementary_PID: ' + elementary_PID)
-                    parameters[i].append(int(stream_type, 16))
-                    parameters[i].append(int(elementary_PID, 16))
-                    if stream_type == hex(2):
+                    stream_type =  int(stream.find('{http://www.streamanalyser.com/schema}stream_type').text,16)
+                    elementary_PID =  int(stream.find('{http://www.streamanalyser.com/schema}elementary_PID').text,16)
+                    for streamIdDescriptor in stream.findall('{http://www.streamanalyser.com/schema}StreamIdentifierDescriptor'):
+                        component_tag = int(streamIdDescriptor.find('{http://www.streamanalyser.com/schema}component_tag').text,16)
+                    print ('STREAM STANDARD: ' + str(component_tag))
+                    print ('stream_type: ' + str(stream_type))
+                    print ('elementary_PID: ' + str(elementary_PID))
+                    
+                    #find type of stream and insert the row in corresponent table and row in streams after that relating both
+                    if stream_type == 2:
                         print ('VIDEO STREAM')
-                        for streamIdDescriptor in stream.findall('{http://www.streamanalyser.com/schema}StreamIdentifierDescriptor'):
-                                component_tag = hex(int(streamIdDescriptor.find('{http://www.streamanalyser.com/schema}component_tag').text,16))
-                                print ('COMPONENT TAG: ' + component_tag)
-                    elif stream_type == hex(3) or stream_type == hex(4):
+                        #insert video table row
+                        idVideo +=1
+                        insertStatement = "INSERT INTO Stream (Elementary_PID, Stream_Type, Stream_Standard, PMT_idPMT, PMT_TS_identifierTS, Video_idVideo) VALUES ({0}, {1}, {2}, {3}, {4}, {5})".format(elementary_PID, stream_type, component_tag, idPMT, idTS, idVideo)
+                        cursor.execute(insertStatement)
+                        idStream +=1
+                    elif stream_type == 3 or stream_type == 4:
                         print ('AUDIO STREAM')
-                        for streamIdDescriptor in stream.findall('{http://www.streamanalyser.com/schema}StreamIdentifierDescriptor'):
-                                component_tag = hex(int(streamIdDescriptor.find('{http://www.streamanalyser.com/schema}component_tag').text,16))
-                                print ('COMPONENT TAG: ' + component_tag)
                         for languageDescriptor in stream.findall('{http://www.streamanalyser.com/schema}ISO639LanguageDescriptor'):
                                 for language in languageDescriptor.findall('{http://www.streamanalyser.com/schema}Language'):
                                         audio_language = language.find('{http://www.streamanalyser.com/schema}ISO_639_language_code').text
-                                        audio_type = hex(int(language.find('{http://www.streamanalyser.com/schema}audio_type').text,16))
+                                        audio_type = int(language.find('{http://www.streamanalyser.com/schema}audio_type').text,16)
                                         print ('AUDIO LANGUAGE: ' + audio_language)
-                                        print ('AUDIO TYPE: ' + audio_type)
-                    elif stream_type == hex(6):
-                        for streamIdDescriptor in stream.findall('{http://www.streamanalyser.com/schema}StreamIdentifierDescriptor'):
-                                component_tag = hex(int(streamIdDescriptor.find('{http://www.streamanalyser.com/schema}component_tag').text,16))
-                                print ('STREAM STANDARD: ' + component_tag)
+                                        print ('AUDIO TYPE: ' + str(audio_type))
+                        insertStatement = "INSERT INTO Audio (Audio_Type, Audio_Language) VALUES ({0}, {1})".format(audio_type, audio_language)
+                        cursor.execute(insertStatement)
+                        idAudio +=1
+                        insertStatement = "INSERT INTO Stream (Elementary_PID, Stream_Type, Stream_Standard, PMT_idPMT, PMT_TS_identifierTS, Audio_idAudio) VALUES ({0}, {1}, {2}, {3}, {4}, {5})".format(elementary_PID, stream_type, component_tag, idPMT, idTS, idAudio)
+                        cursor.execute(insertStatement)
+                        idStream +=1
+
+                    elif stream_type == 6:
                         for child in stream:
                                 if child.tag == '{http://www.streamanalyser.com/schema}SubtitlingDescriptor':
                                         print ('SUBTITLES STREAM')
                                         for subDescriptor in stream.findall('{http://www.streamanalyser.com/schema}SubtitlingDescriptor'):
                                                 for subtitle in subDescriptor.findall('{http://www.streamanalyser.com/schema}Subtitle'):
                                                         subtitle_language = subtitle.find('{http://www.streamanalyser.com/schema}ISO_639_language_code').text
-                                                        subtitle_type = hex(int(subtitle.find('{http://www.streamanalyser.com/schema}subtitling_type').text,16))
+                                                        subtitle_type = int(subtitle.find('{http://www.streamanalyser.com/schema}subtitling_type').text,16)
                                                         print ('SUBTITLE LANGUAGE: ' + subtitle_language)
-                                                        print ('SUBTITLE TYPE: ' + subtitle_type)
+                                                        print ('SUBTITLE TYPE: ' + str(subtitle_type))
+                        insertStatement = "INSERT INTO Subtitles (Subtitles_Language) VALUES ({0})".format(subtitle_language)
+                        cursor.execute(insertStatement)
+                        idSubtitle +=1
+                        insertStatement = "INSERT INTO Stream (Elementary_PID, Stream_Type, Stream_Standard, PMT_idPMT, PMT_TS_identifierTS, Subtitles_idSubtitles) VALUES ({0}, {1}, {2}, {3}, {4}, {5})".format(elementary_PID, stream_type, component_tag, idPMT, idTS, idSubtitle)
+                        cursor.execute(insertStatement)
+                        idStream +=1
+
                     print ('##### END STREAM #####')
-            i +=1
             print ('########### END PMT ##########')
             #VIDEO SECTION
             for video in root.findall('{http://www.streamanalyser.com/schema}Video'):
                 print ('########## START VIDEO ###########')
-                elementary_PID = hex(int(video.find('{http://www.streamanalyser.com/schema}PID').text,16))
-                video_type = hex(int(video.find('{http://www.streamanalyser.com/schema}Type').text,16))
+                elementary_PID = int(video.find('{http://www.streamanalyser.com/schema}PID').text,16)
+                video_type = int(video.find('{http://www.streamanalyser.com/schema}Type').text,16)
                 for info in video.findall('{http://www.streamanalyser.com/schema}Info'):
                     width = int(info.find('{http://www.streamanalyser.com/schema}Width').text)
                     height = int(info.find('{http://www.streamanalyser.com/schema}Height').text)
-                print ('VIDEO PID: ' + elementary_PID)
-                print ('VIDEO TYPE: ' + video_type)
+                print ('VIDEO PID: ' + str(elementary_PID))
+                print ('VIDEO TYPE: ' + str(video_type))
                 print ('RESOLUTION: ' + str(width) + ', ' + str(height))
                 print ('########## END VIDEO ############')
         print ('########################## END TS ####################################')
 
-print (parameters)
-sql_comm= """INSERT INTO PMT (PID_number, Stream_Type, Elementary_PID) VALUES (%d, %d, %d);"""
-result  = cursor.executemany(sql_comm, parameters)
 connection.commit()
-
-
-
 cursor.close()
 connection.close()
 print ('####### SCRIPT END ##########')
