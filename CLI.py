55486760d3055902dc6aeb6f7a40deb5c8080c01
Ruben Monferrer <rubenmonfe@gmail.com>
2019-03-31 18:55:17 +0200

    Implemented command line inteface

---
+++ CLI.py
@@ -0,0 +1,24 @@
+import argparse
+from QueryModule import *
+from SearchModule import *
+import ast
+
+
+# This function parses the arguments and returns a searchDict
+def parseArguments(filterDict):
+	searchDict = {}
+	parser = argparse.ArgumentParser(description='Search for TS that match a criteria')
+	for table_name, column_name in filterDict:
+		parser.add_argument('--'+column_name, help='filter by '+column_name)
+		
+	args = vars(parser.parse_args())
+	for table_name, column_name in filterDict:
+		if args[column_name]:
+			searchDict[(table_name,column_name)] = args[column_name]
+	return searchDict
+
+
+#filterDict = ast.literal_eval(open("filterDict2.txt", "r").read())
+filterDict = obtainFilterDictMT()
+searchDict = parseArguments(filterDict)
+searchResult = querySearchMT(searchDict)
