26520cb14e62010d1e33cfca03e4da204d2331c4
EnricArsu <47326899+EnricArsu@users.noreply.github.com>
2019-05-06 17:03:25 +0200

    More comments

--- QueryModule.py
+++ QueryModule.py
@@ -1,4 +1,4 @@
-#Implements the functions that will interact with the DataBase
+# Implements the functions that will interact with the DataBase.
 
 import mysql.connector
 
@@ -13,74 +13,76 @@ charSet = "utf8mb4"
 
 ######## PRIVATE FUNCTIONS ########
 
-# Auxiliary private function that encapsulates queries to the DB
+# Auxiliary private function that encapsulates queries to the DB.
 def _queryDB(sqlQuery,dbName):
-    # Create a connection object
+    # Create a connection object.
     connectionObject = mysql.connector.connect(host=dbServerName, user=dbUser, password=dbPassword,
                                          db=dbName, charset=charSet)
     try:
-        # Create a cursor object
+        # Create a cursor object.
         cursorObject = connectionObject.cursor()
 
-        # Execute the sqlQuery
+        # Execute the sqlQuery.
         cursorObject.execute(sqlQuery)
 
-        # Fetch all the rows
+        # Fetch all the rows.
         return cursorObject.fetchall()
     
     except Exception as e:
-        #Prints the error if occured
+        # Prints the error if occured.
         print("Exception occured:{}".format(e))
 
     finally:
-        #Always close the conection object
+        # Always close the conection object.
         connectionObject.close()
 
-#Auxiliary function that checks whether the column_name is a parameter that the user wants to filter by.
+# Auxiliary function that checks whether the column_name is a parameter that the user wants to filter by.
 def _obtainIsFilter():
-    #Creates an empty dictionary
+    # Creates an empty dictionary.
     isFilter = {}
 
-    #Gets the information from Filters table
+    # Gets the information from Filters table
     sqlQuery = "SELECT * FROM Filters"
     rows = _queryDB(sqlQuery,dictdbName)
     
-    #Iterates in rows and gets the useful parameters from use.
+    # Iterates in rows and gets the useful parameters from use.
     for column_name, use in rows:
         isFilter[column_name] = use
     
-    #Returns the parameters in the dictionary isFilter
+    # Returns the parameters in the dictionary isFilter.
     return isFilter
 
-# Auxiliary function that returns the tables information 
+# Auxiliary function that returns the tables information. 
 def _obtainTables():
 
-    # Obtain table_name, column_name, column_key of all columns in DB
+    # Obtain table_name, column_name, column_key of all columns in DB.
     sqlQuery = "SELECT table_name, column_name, column_key FROM information_schema.columns WHERE table_schema = '{0}' order by table_name".format(dbName)
 
-    # Execute the sqlQuery and get answer
+    # Execute the sqlQuery and get answer.
     return _queryDB(sqlQuery,dbName)
 
-#Function to obtain a dictionary to convert the values from the database to a user friendly ones.
+# Function to obtain a dictionary to convert the values from the database to a user friendly ones.
 def _obtainConversionDict():
     conversionDict = {}
 
-    # Obtain table_name, column_name, value_name, UserFriendly_value of all columns in DB
+    # Obtain table_name, column_name, value_name, UserFriendly_value of all columns in DB.
     sqlQuery = "SELECT type, var_obtained, value, meaning FROM Dictionary"
     rows = _queryDB(sqlQuery,dictdbName)
+    # Iterates rows in order to convert the values to the user friendly ones.
     for table_name, column_name, value_name, userFriendly_value in rows:
+        # Checks if the value is a digit, because SQL saves it as string and adds the "'" character.
         if value_name.strip("'").isdigit(): 
             conversionDict[(table_name,column_name,int(value_name.strip("'")))] = userFriendly_value
         else:
             conversionDict[(table_name,column_name,value_name)] = userFriendly_value
-
+    # Returns the dictionary.
     return conversionDict
 
-#Function that given a user friendly value, it returns the corresponding parameter in the database.
+# Function that given a user friendly value, it returns the corresponding parameter in the database.
 def _obtainInvConversionDict():
     invConversionDict = {}
 
-    # Obtain table_name, column_name, value_name, UserFriendly_value of all columns in DB
+    # Obtain table_name, column_name, value_name, UserFriendly_value of all columns in DB.
     sqlQuery = "SELECT type, var_obtained, value, meaning FROM Dictionary"
     rows = _queryDB(sqlQuery,dictdbName)
     for table_name, column_name, value_name, userFriendly_value in rows:
@@ -94,20 +96,20 @@ def _obtainInvConversionDict():
 
 
 
-#This function returns a dictionary containing all the fields and their corresponding values to search for.
+# This function returns a dictionary containing all the fields and their corresponding values to search for.
 def obtainFilterDict():
     
-    # Obtain PKInfo()
+    # Obtain table information.
     tableInfo = _obtainTables()
 
-    # Obtain isFilter dictionary
+    # Obtain isFilter dictionary.
     isFilter = _obtainIsFilter()
 
-    # Obtain isFilter dictionary
+    # Obtain convertion dictionary.
     conversionDict = _obtainConversionDict()
 
-    # Create dictionary to store db info
-    # Tuple of (Table,Column) as key and list of distinct values for the column as value
+    # Create dictionary to store db info.
+    # Tuple of (Table,Column) as key and list of distinct values for the column as value.
     filterDict = {}
     for table_name, column_name, column_key in tableInfo:
         if isFilter[column_name]:
@@ -122,7 +124,7 @@ def obtainFilterDict():
                         filterDict[(table_name,column_name)].append(value)
     return filterDict
 
-
+# This function makes the query to the database by obtaining the dictionary with the fields and corresponding values to search, chosen by the user.
 def querySearch(searchDict, urlsFlag=False):
     
     conversionDict = _obtainConversionDict()
@@ -162,13 +164,13 @@ def querySearch(searchDict, urlsFlag=False):
     else:
         sqlQuery = "SELECT Path, Service_Name FROM PMT NATURAL JOIN TS WHERE idPMT IN ({0})".format(sqlQuery)
     
-    # Execute the sqlQuery and get answer in rows
+    # Execute the sqlQuery and get answer in rows.
     rows = _queryDB(sqlQuery,dbName)
     
-    # Create dictionary to store result info
+    # Create dictionary to store result info.
     resultDict = {}
 
-    # Dictionary format with urls: resultDict[identifierTS][Service_Name] = [url1,url2,...]
+    # Dictionary format with urls: resultDict[Path][Service_Name] = [url1,url2,...].
     if urlsFlag:
         for Path, Service_Name, URL in rows:
             if ('PMT', 'Service_Name', Service_Name) in conversionDict:
@@ -181,7 +183,7 @@ def querySearch(searchDict, urlsFlag=False):
                 resultDict[Path][convertedService_Name] = []
             resultDict[Path][convertedService_Name].append(URL)
     
-    # Dictionary format without urls: resultDict[Path] = [Service_Name1,Service_Name2,...]
+    # Dictionary format without urls: resultDict[Path] = [Service_Name1,Service_Name2,...].
     else:
         for Path, Service_Name in rows:
             if Path not in resultDict:
