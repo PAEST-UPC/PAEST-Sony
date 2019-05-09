078e97584962a2524a22a800ba4984ba20927ffb
EnricArsu <47326899+EnricArsu@users.noreply.github.com>
2019-05-09 17:02:06 +0200

    Comments and CACHE Time

--- CLI.py
+++ CLI.py
@@ -2,6 +2,7 @@ import argparse, sys, pandas, ast, os, time
 from QueryModule import *
 from SearchModule import *
 
+CACHE_TIME = 3600
 
 def main():
     _xml_dir_path = r'/home/ubuntu/pae/xml/StreamAnalyzer'
@@ -34,9 +35,11 @@ def main():
             df.to_csv('searchResult.csv')
 
 
-# This function parses the arguments and returns a searchDict
+# This function parses the arguments and returns a searchDict.
 def _parseArguments(filterDict):
     searchDict = {}
+    
+    # Defines and describes the arguments of the helper.
     parser = argparse.ArgumentParser(description='Search for TS that match a criteria')
     for table_name, column_name in filterDict:
         parser.add_argument('--'+column_name, help=f'Filter by {column_name}. Current available options: {filterDict[(table_name,column_name)]}')
@@ -44,8 +47,11 @@ def _parseArguments(filterDict):
     parser.add_argument('--getUrls', '-u', help='If you add this argument the results will include urls if possible', default=False, dest='getUrls', action='store_true')   
     parser.add_argument('--exportCsv', '-csv', help='If you add this argument the results will be exported to a csv file', default=False, dest='exportCsv', action='store_true')   
     
+    # Checks if there are arguments to search. If there aren't any returns error.
     if len(sys.argv) < 2:
         parser.error('Missing arguments')
+        
+    # If there is only one argument and no value to search returns all possible values for that argument.
     if len(sys.argv) == 2:
         for table_name, column_name in filterDict:
             if sys.argv[1] == '--'+column_name:
@@ -69,17 +75,23 @@ def _parseArguments(filterDict):
             searchDict[(table_name,column_name)] = args[column_name]
 
     return searchDict, urlsFlag, csvFlag
-
+    
+# This function creates a .txt file where the filters are saved as a cache to save unnecessary updates to database.
 def getFilterDict():
     filename = 'filterDict.txt'
     filterDict = {}
+    # Checks if the path contains the file.
     if os.path.isfile(filename):
-        if time.time() - os.path.getmtime(filename) < 120:
+        # Calculates the file time by calculating time.time() that returns the time in seconds since the epoch, and substracts the time of last modification of file with os.path.getmtime(filename.)
+        file_time=time.time() - os.path.getmtime(filename)
+        # Checks how old is the file. 
+        if file_time < CACHE_TIME:
+            # Opens a file given by filename in read mode. Doesn't need to be closed because of the with-as structure.
             with open(filename, "r") as data:
                 filterDict = ast.literal_eval(data.read())
             return filterDict
     
-
+    # In case that the time exceeds CACHE_TIME obtains the latest filter dictionary and overwrites the old one.
     filterDict = obtainFilterDict()
     with open(filename, "w") as file:
         file.write(str(filterDict))
@@ -88,5 +100,3 @@ def getFilterDict():
 
 if __name__ == "__main__":
     main()
-
-    
--- SearchModule.py
+++ SearchModule.py
@@ -4,15 +4,15 @@ import re,os
 
 ######## PRIVATE FUNCTIONS ########
 
-# return True if a pattern is contained in a line
+# Return True if a pattern is contained in a line.
 def _findLine(pattern,line):
-    match = re.search(pattern,line)                 #Returns a match object if pattern is contained in line. If it doesn't match it returns a None object. 
+    match = re.search(pattern,line)                 # Returns a match object if pattern is contained in line. If it doesn't match it returns a None object. 
     if match:                               
         return True
     return False
-# return True if a pattern is contained in a File
+# Return True if a pattern is contained in a File.
 def _findFile(pattern,filePath):            
-    with open(filePath) as file:                    #Opens a file given by filePath. Doesn't need to be closed because of the with-as structure.
+    with open(filePath) as file:                    # Opens a file given by filePath. Doesn't need to be closed because of the with-as structure.
         for line in file:
             if _findLine(pattern,line):
                 return True
@@ -21,17 +21,17 @@ def _findFile(pattern,filePath):
 
 ######## PUBLIC FUNCTIONS ########
 
-# return a list of all files in a directory (recursive for subdirectories) that contain a pattern
+# Return a list of all files in a directory (recursive for subdirectories) that contain a pattern.
 def searchText(pattern,dirPath):
-    matchList = []                                  #Creates a list where all the files that contain the pattern will be saved.
+    matchList = []                                  # Creates a list where all the files that contain the pattern will be saved.
 
     for fileName in os.listdir(dirPath):
-        path = os.path.join(dirPath,fileName)       #Given a dirPath and a fileName creates a path.
-        if os.path.isfile(path):                    #Checks if the path is a file.
+        path = os.path.join(dirPath,fileName)       # Given a dirPath and a fileName creates a path.
+        if os.path.isfile(path):                    # Checks if the path is a file.
             if _findFile(pattern,path):
-                matchList.append(fileName)          #Appends the fileName to the matchList.
-        elif os.path.isdir(path):                   #Checks if the path is a directory.
-            matchList += searchText(pattern,path)   #Calls itself again until it finds a file.
+                matchList.append(fileName)          # Appends the fileName to the matchList.
+        elif os.path.isdir(path):                   # Checks if the path is a directory.
+            matchList += searchText(pattern,path)   # Calls itself again until it finds a file.
     return matchList
 
 
